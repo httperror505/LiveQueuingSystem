c1005a12cc59144b5c0e87a3e279422a082f260a
John Rey Tolosa <johnrey.tolosa2@gmail.com>
2025-02-17 13:20:32 +0800

    Add BodyClass

--- QueuieSytems/Program.cs
+++ QueuieSytems/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using QueuieSytems;
+using QueuieSytems.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
---
+++ QueuieSytems/Services/BodyClassService.cs
@@ -0,0 +1,18 @@
+﻿namespace QueuieSytems.Services
+{
+    public class BodyClassService
+    {
+
+    private string _currentClass = "";
+
+        public event Action<string>? OnClassChanged;
+
+        public void SetBodyClass(string className)
+        {
+            _currentClass = className;
+            OnClassChanged?.Invoke(className);
+        }
+
+        public string GetBodyClass() => _currentClass;
+    }
+}
